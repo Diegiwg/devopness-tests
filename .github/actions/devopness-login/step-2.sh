4f4a4ebdfdacba0c6a5ae9c98605a9352fbdbf3c
Diegiwg <diegiwg@gmail.com>
2025-02-22 21:34:56 -0300

    feat: add cleanup step for temporary files and update token output method in Devopness login action

--- .github/actions/devopness-login/step-2.sh
+++ .github/actions/devopness-login/step-2.sh
@@ -14,4 +14,5 @@ fi
 encoded_token=$(echo -n "$access_token" | base64 | tr -d '\n')
 echo "::add-mask::$encoded_token"
 
-echo "::set-output name=devopness_token::$encoded_token"
+# echo "::set-output name=devopness_token::$encoded_token"
+echo "devopness_token=$encoded_token" >>"$GITHUB_OUTPUT"
